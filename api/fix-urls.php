9874647ee3218e89961b2e7c74197919960f1e0b
TecnoCenter Sistemas <tecnocentersistemas@gmail.com>
2026-02-21 02:21:13 -0300

    Fix: aggressive https url fix

--- api/fix-urls.php
+++ api/fix-urls.php
@@ -1,21 +1,17 @@
 <?php
 // One-time fix: update all song metadata to use https
+header('Content-Type: application/json');
 $metaDir = __DIR__ . '/../data/songs';
 $files = glob($metaDir . '/*.json');
-$fixed = 0;
+$results = [];
 foreach ($files as $f) {
-    $data = json_decode(file_get_contents($f), true);
-    if (!$data) continue;
-    $changed = false;
-    foreach (['audioUrl', 'videoUrl', 'imageUrl', 'shareUrl'] as $key) {
-        if (!empty($data[$key]) && strpos($data[$key], 'http://') === 0) {
-            $data[$key] = str_replace('http://', 'https://', $data[$key]);
-            $changed = true;
-        }
-    }
-    if ($changed) {
-        file_put_contents($f, json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
-        $fixed++;
+    $raw = file_get_contents($f);
+    $newRaw = str_replace('http://cristianos.centralchat.pro', 'https://cristianos.centralchat.pro', $raw);
+    if ($raw !== $newRaw) {
+        file_put_contents($f, $newRaw);
+        $results[] = basename($f) . ' FIXED';
+    } else {
+        $results[] = basename($f) . ' already ok';
     }
 }
-echo json_encode(['fixed' => $fixed, 'total' => count($files)]);
+echo json_encode(['files' => $results, 'sample' => substr(file_get_contents($files[0] ?? ''), 0, 300)]);
